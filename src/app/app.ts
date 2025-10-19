9a459d7fae62075b0782572832a14eb81d992a7f
ghstll <129940439+ghstll@users.noreply.github.com>
2025-10-19 10:25:02 -0600

    changes styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProductoCard } from './components/producto-card/producto-card';
-import { ProductosLista } from "./components/productos-lista/productos-lista";
 import { HeaderMenu } from "./header-menu/header-menu";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductoCard, ProductosLista, HeaderMenu],
+  imports: [RouterOutlet, HeaderMenu],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
